ea9269118148bc0ef3a44c98b7fc5534d003d55e
Sankajith D. Jinasena <22cds0431@ms.sab.ac.lk>
2026-01-11 10:00:02 +0530

    create bat file

---
+++ app.bat
@@ -0,0 +1,5 @@
+@echo off
+cd E:\Project\AnimalDetector\py_file
+
+start "" "detect.bat"
+
---
+++ py_file/detect.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Run the Python script
+python detect.py
+
+:: Keep the window open if the script crashes or closes
+pause
